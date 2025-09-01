4ba9ca3ea7c2ff10cb1b6c422efffc2d85aa0264
karan-jhaa <kjha32990@gmail.com>
2025-09-01 10:58:13 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+// createRoot(document.getElementById("root")).render(
+//   <div>
+//     <h1>my fav food</h1>
+//     <ul>
+//       <li>banana</li>
+//       <li>banana</li>
+//       <li>banana</li>
+//     </ul>
+//   </div>,
+// );
+
+
+// const currentyear = new Date().getFullYear();
+
+
+
+
